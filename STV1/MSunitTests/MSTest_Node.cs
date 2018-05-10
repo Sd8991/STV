b0fb7cef22f63086fd982f93def7d0ea47a3609b
Rick Goossens <r.goossens2@students.uu.nl>
2018-05-10 15:51:09 +0200

    MSTest_Node (partially) & MSTest_Player (As good as done I think?)

---
+++ STV1/MSunitTests/MSTest_Node.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STVRogue.GameLogic
+{
+    [TestClass]
+    class MSTest_Node
+    {
+        [TestMethod]
+        public void MSTest_connect_nodes()
+        {
+            Node baseNode = new Node();
+            Node nbNode = new Node();
+            baseNode.connect(nbNode);
+            Assert.IsTrue(baseNode.neighbors.Contains(nbNode) && nbNode.neighbors.Contains(baseNode));
+        }
+
+        [TestMethod]
+        public void MSTest_disconnect_nodes()
+        {
+            Node baseNode = new Node();
+            Node nbNode = new Node();
+            baseNode.neighbors.Add(nbNode);
+            baseNode.neighbors.Add(baseNode);
+            baseNode.disconnect(nbNode);
+            Assert.IsFalse(baseNode.neighbors.Contains(nbNode) && nbNode.neighbors.Contains(baseNode));
+        }
+
+        [TestMethod]
+        public void MSTest_contested_node()
+        {
+            Player P = new Player();
+            Pack pack = new Pack("pack", 2);
+            Node N = new Node();
+            P.location = N;
+            pack.location = N;
+            Assert.IsTrue(N.contested(P));
+        }
+
+        [TestMethod]
+        public void MSTest_not_contested_node()
+        {
+            Player P = new Player();
+            Pack pack = new Pack("pack", 2);
+            Node N = new Node();
+            P.location = N;
+            Assert.IsFalse(N.contested(P));
+        }
+    }
+}
--- STV1/MSunitTests/MSTest_Player.cs
+++ STV1/MSunitTests/MSTest_Player.cs
@@ -12,6 +12,15 @@ namespace STVRogue.GameLogic
     public class MSTest_Player
     {
         [TestMethod]
+        public void MStest_create_player()
+        {
+            Player P = new Player();
+            Assert.IsTrue(P.id == "player");
+            Assert.IsTrue(P.AttackRating == 5);
+            //Maybe other values too?
+        }
+
+        [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void MSTest_use_onEmptyBag()
         {
@@ -27,6 +36,65 @@ namespace STVRogue.GameLogic
             P.bag.Add(x);
             P.use(x);
             Assert.IsFalse(P.bag.Contains(x));
+        }
+
+        [TestMethod]
+        public void MSTest_become_accelerated()
+        {
+            Player P = new Player();
+            Item x = new Crystal("cry1");
+            P.bag.Add(x);
+            P.use(x);
+            Assert.IsFalse(P.bag.Contains(x));
+            Assert.IsTrue(P.accelerated);
+        }
+
+        [TestMethod]
+        public void MSTest_test_base_creature_attack()
+        {
+            Player P = new Player();
+            Creature M = new Monster("orc1");
+            int preHP = M.HP;
+            P.Attack(M);
+            Assert.IsTrue(M.HP < preHP);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void MSTest_attack_non_foe()
+        {
+            Player P = new Player();
+            Creature M = new Player();
+            P.Attack(M);
+        }
+
+        [TestMethod]
+        public void MSTest_remove_monster_from_pack_non_acc()
+        {
+            Player P = new Player();
+            Pack pack = new Pack("pack", 2);
+            pack.members[0].HP = 1;
+            Monster M = pack.members[0];
+            pack.startingHP = 1 + pack.members[1].HP;
+            P.Attack(pack.members[0]);
+            Assert.IsTrue(!pack.members.Contains(M));
+        }
+
+        [TestMethod]
+        public void MSTest_check_dead_monsters()
+        {
+            Player P = new Player();
+            Pack pack = new Pack("pack", 4);
+            pack.members[1].HP = 1;
+            pack.members[2].HP = 1;
+            pack.members[3].HP = 7;
+            Monster M1 = pack.members[1];
+            Monster M2 = pack.members[2];
+            Monster M3 = pack.members[3];
+            P.accelerated = true;
+            P.Attack(pack.members[0]);
+            Assert.IsTrue(pack.members.Contains(M3));
+            Assert.IsFalse(pack.members.Contains(M1) && pack.members.Contains(M2));
         }
     }
 }
