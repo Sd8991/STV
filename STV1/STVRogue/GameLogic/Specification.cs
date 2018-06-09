23c4abb541caff01f04ea8a21ea172381f69be4b
S-Schroevers <S.Schroevers@students.uu.nl>
2018-06-09 16:08:54 +0200

    always and unless added

--- STV1/STVRogue/GameLogic/Specification.cs
+++ STV1/STVRogue/GameLogic/Specification.cs
@@ -10,4 +10,32 @@ namespace STVRogue.GameLogic
 	{
 		public abstract bool Test(Game g);
 	}
+
+	class Always : Specification
+	{
+		private Predicate<Game> p;
+		public Always(Predicate<Game> p) { this.p = p; }
+		public override bool Test(Game g){ return p(g); }
+	}
+	class Unless : Specification
+	{
+		private Predicate<Game> p;
+		private Predicate<Game> q;
+		List<bool> history;
+		public Unless(Predicate<Game> p, Predicate<Game> q) { this.p = p; this.q = q; }
+
+		public override bool Test(Game g)
+		{
+			bool verdict;
+			if (history.Count >= 1)
+			{
+				bool previous = history.Last();
+				verdict = !previous || (previous && (p(g) || q(g));
+			}
+			else
+				verdict = p(g) && !q(g);
+			history.Add(p(g) && !q(g));
+			return verdict;
+		}
+	}
 }
