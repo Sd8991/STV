12f03970ea14b9ccf8d76815405a5086a18e5e29
S-Schroevers <S.Schroevers@students.uu.nl>
2018-06-11 16:21:30 +0200

    inputparser infrastructure

--- STV1/STVRogue_Main/Program.cs
+++ STV1/STVRogue_Main/Program.cs
@@ -14,15 +14,23 @@ namespace STVRogue
         static void Main(string[] args)
         {
             Game game = new Game(5, 2, 20);
+			RecordGamePlay recording = new RecordGamePlay(game);
             int width = Console.LargestWindowWidth;
             int height = Console.LargestWindowHeight;
             Console.SetWindowSize(width, height);
             while (true)
             {
                 game.ui.drawUI(game.dungeon, game.player);
-                Console.ReadKey();
-                game.update(new Command());
+				string input = Console.ReadLine();
+				Command command = ParseInput(input);
+				recording.RecordTurn(command);
+                game.update(command);
             }
         }
+
+		static Command ParseInput(string input)
+		{
+			return new Command();
+		}
     }
 }
